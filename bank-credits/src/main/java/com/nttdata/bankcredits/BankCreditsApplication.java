44b6c29e3f3e019bdd45782500fbc14272823be6
javilafoure87 <javilafoure@hotmail.com>
2023-02-14 10:49:50 -0500

    add service credits

---
+++ bank-credits/src/main/java/com/nttdata/bankcredits/BankCreditsApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bankcredits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCreditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCreditsApplication.class, args);
+	}
+
+}
---
+++ bank-credits/src/test/java/com/nttdata/bankcredits/BankCreditsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bankcredits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCreditsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
