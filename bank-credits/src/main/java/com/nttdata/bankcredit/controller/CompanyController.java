b07028eebca1f8ec8e02fe69ea093ad924d7350a
javilafoure87 <javilafoure@hotmail.com>
2023-02-15 22:10:56 -0500

    *customer update*

--- bank-credits/src/main/java/com/nttdata/bankcredit/controller/CompanyController.java
+++ bank-credits/src/main/java/com/nttdata/bankcredit/controller/CompanyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/company")
+@RequestMapping(value = "/api/company")
 public class CompanyController {
 
     @Autowired
