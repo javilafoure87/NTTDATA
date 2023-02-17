72f6e8b5f1d2111435787f6158289bdf47c07c76
@Alexanderd20 <asalas@unsa.edu.pe>
2023-02-17 12:45:46 -0500

    update bank-project

--- bank-account/src/main/java/com/microservice/bankaccount/controller/DepositAccountController.java
+++ bank-account/src/main/java/com/microservice/bankaccount/controller/DepositAccountController.java
@@ -4,10 +4,12 @@ import com.microservice.bankaccount.entity.DepositAccountEntity;
 import com.microservice.bankaccount.service.DepositAccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Optional;
 
@@ -21,14 +23,13 @@ public class DepositAccountController {
     @Autowired
     private DepositAccountService depositAccountService;
 
+    /**
+     * @ResponseStatus(HttpStatus.OK)
+     * public void saveDepositAccount(@RequestBody DepositAccountEntity depositAccountEntity){
+      * depositAccountService.saveDepositAccount(depositAccountEntity);
+     *}
+     */
 
-   /***
-    * @PostMapping
-    ** ResponseStatus(HttpStatus.OK)
-    ** public void saveDepositAccount(@RequestBody DepositAccountEntity depositAccountEntity){
-    *       depositAccountService.saveDepositAccount(depositAccountEntity);
-    * }
-    */
 
     @PostMapping("/deposit/{id}")
     public boolean savingDepositAccountAvailable(@RequestBody String id) {
@@ -37,6 +38,4 @@ public class DepositAccountController {
         depositAccount.orElseThrow(() -> new RuntimeException("Cannot find Saving Account for the client" + id));
         return depositAccount.get().getAmount() > 0;
     }
-
-
 }
